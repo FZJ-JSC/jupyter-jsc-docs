028751f5a70f9a97f6d7eb56b92ab299ae7e4919
kreuzer <t.kreuzer@fz-juelich.de>
2022-01-04 15:27:07 +0100

    update functional tests

--- tests/functional_tests/tests.py
+++ tests/functional_tests/tests.py
@@ -1,215 +1,21 @@
+import base64
 import copy
 import json
 import os
-import time
 import unittest
 
-import pytest
 import requests
-from kubernetes import client
-from kubernetes.stream import stream
 from parameterized import parameterized
-
-
-def load_env():
-    suffix = os.environ.get("CI_COMMIT_SHORT_SHA", "")
-    name = f"tunneling-devel-{suffix}"
-    namespace = "gitlab"
-    image = (
-        f"registry.jsc.fz-juelich.de/jupyterjsc/k8s/images/tunneling-relaunch:{suffix}"
-    )
-    k8s_host = os.environ.get("K8S_TEST_CLUSTER_SERVER", "")
-    k8s_user_token = os.environ.get("K8S_TEST_CLUSTER_USER_TOKEN", "")
-    k8s_ca_auth_path = os.environ.get("CA_AUTH_PATH")
-    url = f"http://{name}:8080/api"
-    return (
-        suffix,
-        name,
-        namespace,
-        image,
-        k8s_host,
-        k8s_user_token,
-        k8s_ca_auth_path,
-        url,
-    )
-
-
-def load_k8s_client(k8s_host, k8s_user_token, k8s_ca_auth_path):
-    conf = client.Configuration()
-    conf.host = k8s_host
-    conf.api_key["authorization"] = k8s_user_token
-    conf.api_key_prefix["authorization"] = "Bearer"
-    conf.ssl_ca_cert = k8s_ca_auth_path
-    return client.CoreV1Api(client.ApiClient(conf))
-
-
-def get_svc_manifest(name, port, suffix=""):
-    return {
-        "apiVersion": "v1",
-        "kind": "Service",
-        "metadata": {
-            "labels": {
-                "name": f"{name}{suffix}",
-            },
-            "name": f"{name}{suffix}",
-            "resourceversion": "v1",
-        },
-        "spec": {
-            "ports": [
-                {
-                    "name": "port",
-                    "port": port,
-                    "protocol": "TCP",
-                    "targetPort": port,
-                }
-            ],
-            "selector": {"name": name},
-        },
-    }
-
-
-def delete_tunneling_pod_and_svcs(v1, name, namespace):
-    v1.delete_namespaced_service(name=name, namespace=namespace)
-    v1.delete_namespaced_service(name=f"{name}-ssh", namespace=namespace)
-    v1.delete_namespaced_pod(name=name, namespace=namespace)
-
-
-def start_tunneling_pod_and_svcs(v1, name, namespace, image):
-    pod_manifest = {
-        "apiVersion": "v1",
-        "kind": "Pod",
-        "metadata": {
-            "labels": {
-                "name": name,
-            },
-            "name": name,
-            "resourceversion": "v1",
-        },
-        "spec": {
-            "containers": [
-                {
-                    "image": image,
-                    "imagePullPolicy": "Always",
-                    "name": name,
-                    "env": [
-                        {
-                            "name": "DEPLOYMENT_NAME",
-                            "value": name,
-                        },
-                        {
-                            "name": "DEPLOYMENT_NAMESPACE",
-                            "value": namespace,
-                        },
-                        {
-                            "name": "TUNNEL_SUPERUSER_PASS",
-                            "value": os.environ.get("TUNNEL_SUPERUSER_PASS"),
-                        },
-                        {
-                            "name": "SSHCONFIGFILE",
-                            "value": "/home/tunnel/web/tests/config/functional_tests/ssh_config",
-                        },
-                    ],
-                }
-            ],
-            "serviceAccount": "tunneling-devel-svc-acc",
-            "serviceAccountName": "tunneling-devel-svc-acc",
-        },
-    }
-    v1.create_namespaced_pod(body=pod_manifest, namespace=namespace)
-    while True:
-        resp = v1.read_namespaced_pod(name=name, namespace=namespace)
-        if resp.status.phase != "Pending":
-            break
-        time.sleep(1)
-    v1.create_namespaced_service(body=get_svc_manifest(name, 8080), namespace=namespace)
-    v1.create_namespaced_service(
-        body=get_svc_manifest(name, 2222, suffix="-ssh"), namespace=namespace
-    )
-
-
-def wait_for_tunneling_svc(url):
-    for _ in range(0, 40):
-        try:
-            r = requests.get(url=f"{url}/health/", timeout=2)
-            if r.status_code == 200:
-                break
-        except (
-            requests.exceptions.ConnectTimeout,
-            requests.exceptions.ConnectionError,
-            requests.exceptions.ReadTimeout,
-        ):
-            pass
-        time.sleep(1)
-
-
-def wait_for_tsi_svc(v1, name, namespace):
-    exec_command = ["/bin/sh"]
-    resp = stream(
-        v1.connect_get_namespaced_pod_exec,
-        name,
-        namespace,
-        command=exec_command,
-        stderr=True,
-        stdin=True,
-        stdout=True,
-        tty=False,
-        _preload_content=False,
-    )
-    command = "netstat -ltnp 2>/dev/null | tr -s ' ' | grep \":4433\" | wc -l"
-
-    while resp.is_open():
-        for _ in range(0, 15):
-            resp.update(timeout=1)
-            if resp.peek_stdout():
-                stdout = resp.read_stdout()
-                print("STDOUT: %s" % stdout)
-                if stdout == "1":
-                    break
-            if resp.peek_stderr():
-                print("STDERR: %s" % resp.read_stderr())
-            print("UNICORE TSI not running yet")
-            print("Running command... %s\n" % command)
-            resp.write_stdin(command + "\n")
-            time.sleep(10)
-    resp.close()
-
-    # ; sleep 10; let COUNTER-=1; fi; done; if [[ $COUNTER -eq 0 ]]; then echo "TSI GateWay not reachable. Exit"; exit 1; fi
-
-
-def start_unicore_tsi_pod_and_svcs(v1, name, namespace, image):
-    pod_manifest = {
-        "apiVersion": "v1",
-        "kind": "Pod",
-        "metadata": {
-            "labels": {
-                "name": name,
-            },
-            "name": name,
-            "resourceversion": "v1",
-        },
-        "spec": {
-            "containers": [
-                {
-                    "image": image,
-                    "imagePullPolicy": "Always",
-                    "name": name,
-                }
-            ],
-            "imagePullSecrets": [{"name": "gitlab-registry"}],
-        },
-    }
-    v1.create_namespaced_pod(body=pod_manifest, namespace=namespace)
-    while True:
-        resp = v1.read_namespaced_pod(name=name, namespace=namespace)
-        if resp.status.phase != "Pending":
-            break
-        time.sleep(1)
-    v1.create_namespaced_service(body=get_svc_manifest(name, 2223), namespace=namespace)
-
-
-def delete_unicore_tsi_pod_and_svcs(v1, name, namespace):
-    v1.delete_namespaced_service(name=name, namespace=namespace)
-    v1.delete_namespaced_pod(name=name, namespace=namespace)
+from utils import add_test_files_to_tunneling
+from utils import delete_tunneling_pod_and_svcs
+from utils import delete_unicore_tsi_pod_and_svcs
+from utils import load_env
+from utils import load_k8s_client
+from utils import replace_ssh_host_in_tsi_manage_tunnel_script
+from utils import start_tunneling_pod_and_svcs
+from utils import start_unicore_tsi_pod_and_svcs
+from utils import wait_for_tsi_svc
+from utils import wait_for_tunneling_svc
 
 
 class FunctionalTests(unittest.TestCase):
@@ -258,13 +64,56 @@ class FunctionalTests(unittest.TestCase):
             url,
         ) = load_env()
         v1 = load_k8s_client(k8s_host, k8s_user_token, k8s_ca_auth_path)
-        start_tunneling_pod_and_svcs(v1, name, namespace, image)
         tsi_version = os.environ.get("UNICORE_TSI_VERSION")
         tsi_image = f"registry.jsc.fz-juelich.de/jupyterjsc/k8s/images/unicore-test-server/unicore-tsi-slurm:{tsi_version}"
         tsi_name = "unicore-test-tsi"
-        start_unicore_tsi_pod_and_svcs(v1, tsi_name, namespace, tsi_image)
-        time.sleep(15)
+        start_unicore_tsi_pod_and_svcs(
+            v1, tsi_name, namespace, tsi_image, f"{name}-ssh"
+        )
+        start_tunneling_pod_and_svcs(v1, name, namespace, image)
+        auth_keys_b64 = base64.b64encode(
+            (
+                '# DemoSite\nrestrict,port-forwarding,command="/bin/echo No commands allowed" '
+                + os.environ.get("LJUPYTER_SSH_TUNNEL_PUBLIC_KEY")
+                + "\n"
+            ).encode("utf-8")
+        ).decode("utf-8")
+        data = [
+            (
+                "/home/tunnel/.ssh/authorized_keys",
+                auth_keys_b64,
+                True,
+                "tunnel:users",
+                "600",
+            ),
+            (
+                "/tmp/ssh_config",
+                os.environ.get("FUNCTIONAL_TESTS_SSH_CONFIG"),
+                True,
+                "",
+                "",
+            ),
+            (
+                "/tmp/remote",
+                os.environ.get("TUNNELSERVICE_SSH_REMOTE_PRIVATE_KEY"),
+                False,
+                "tunnel:users",
+                "400",
+            ),
+            (
+                "/tmp/tunnel",
+                os.environ.get("TUNNELSERVICE_SSH_TUNNEL_PRIVATE_KEY"),
+                False,
+                "tunnel:users",
+                "400",
+            ),
+        ]
+        add_test_files_to_tunneling(v1, name, namespace, data)
+        replace_ssh_host_in_tsi_manage_tunnel_script(
+            v1, tsi_name, namespace, f"{name}-ssh"
+        )
         wait_for_tunneling_svc(url)
+        wait_for_tsi_svc(v1, tsi_name, namespace)
 
     @classmethod
     def tearDownClass(cls):
@@ -279,9 +128,9 @@ class FunctionalTests(unittest.TestCase):
             url,
         ) = load_env()
         v1 = load_k8s_client(k8s_host, k8s_user_token, k8s_ca_auth_path)
-        # delete_tunneling_pod_and_svcs(v1, name, namespace)
+        delete_tunneling_pod_and_svcs(v1, name, namespace)
         tsi_name = "unicore-test-tsi"
-        # delete_unicore_tsi_pod_and_svcs(v1, tsi_name, namespace)
+        delete_unicore_tsi_pod_and_svcs(v1, tsi_name, namespace)
 
     def logtest_stream(self):
         logtest_url = f"{self.url}/logs/logtest/"
@@ -328,7 +177,7 @@ class FunctionalTests(unittest.TestCase):
             ("stream", "json"),
         ]
     )
-    def no_test_stream_handler(self, handler, formatter):
+    def test_stream_handler(self, handler, formatter):
         handler_url = f"{self.url}/logs/handler/"
         stream_url = f"{self.url}/logs/handler/stream/"
         body = copy.deepcopy(self.configurations[handler])
@@ -336,11 +185,11 @@ class FunctionalTests(unittest.TestCase):
 
         # Check that nothing's defined
         r = requests.get(url=handler_url, headers=self.headers, timeout=2)
-        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.status_code, 200, self.headers)
         self.assertEqual(r.json(), [])
 
         # Test stdout with no handler defined
-        logs_1, logs_2 = self.logtest()
+        logs_1, logs_2 = self.logtest_stream()
         self.assertEqual(len(logs_1) + 4, len(logs_2))
         self.assertEqual(logs_2[-4:-1], ["Warn", "Error", "Critical"])
 
@@ -383,7 +232,7 @@ class FunctionalTests(unittest.TestCase):
         self.assertEqual(r.status_code, 200)
 
         # Test Stream handler with TRACE
-        logs_1, logs_2 = self.logtest()
+        logs_1, logs_2 = self.logtest_stream()
         self.assertEqual(len(logs_1) + 7, len(logs_2))
         if formatter == "simple":
             self.assertTrue(logs_2[-7].endswith("function=list : Trace"))
@@ -396,7 +245,7 @@ class FunctionalTests(unittest.TestCase):
         self.assertEqual(r.status_code, 200)
 
         # Test Stream handler with DEACTIVATE
-        logs_1, logs_2 = self.logtest()
+        logs_1, logs_2 = self.logtest_stream()
         self.assertEqual(len(logs_1) + 1, len(logs_2))
 
         # Delete handler
@@ -404,6 +253,13 @@ class FunctionalTests(unittest.TestCase):
         self.assertEqual(r.status_code, 204)
 
         # Test stdout with no handler defined
-        logs_1, logs_2 = self.logtest()
+        logs_1, logs_2 = self.logtest_stream()
         self.assertEqual(len(logs_1) + 4, len(logs_2))
         self.assertEqual(logs_2[-4:-1], ["Warn", "Error", "Critical"])
+
+    def test_remote_tunnel(self):
+        # Get status of ssh remote tunnel
+        remote_url = f"{self.url}/remote/"
+        r = requests.get(url=remote_url, headers=self.headers)
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.json(), [])
---
+++ tests/functional_tests/utils.py
@@ -0,0 +1,268 @@
+import os
+import time
+
+import requests
+from kubernetes import client
+from kubernetes.stream import stream
+
+
+def load_env():
+    suffix = os.environ.get("CI_COMMIT_SHORT_SHA", "")
+    name = f"tunneling-devel-{suffix}"
+    namespace = "gitlab"
+    image = (
+        f"registry.jsc.fz-juelich.de/jupyterjsc/k8s/images/tunneling-relaunch:{suffix}"
+    )
+    k8s_host = os.environ.get("K8S_TEST_CLUSTER_SERVER", "")
+    k8s_user_token = os.environ.get("K8S_TEST_CLUSTER_USER_TOKEN", "")
+    k8s_ca_auth_path = os.environ.get("CA_AUTH_PATH")
+    url = f"http://{name}:8080/api"
+    return (
+        suffix,
+        name,
+        namespace,
+        image,
+        k8s_host,
+        k8s_user_token,
+        k8s_ca_auth_path,
+        url,
+    )
+
+
+def load_k8s_client(k8s_host, k8s_user_token, k8s_ca_auth_path):
+    conf = client.Configuration()
+    conf.host = k8s_host
+    conf.api_key["authorization"] = k8s_user_token
+    conf.api_key_prefix["authorization"] = "Bearer"
+    conf.ssl_ca_cert = k8s_ca_auth_path
+    return client.CoreV1Api(client.ApiClient(conf))
+
+
+def get_svc_manifest(name, port, suffix=""):
+    return {
+        "apiVersion": "v1",
+        "kind": "Service",
+        "metadata": {
+            "labels": {
+                "name": f"{name}{suffix}",
+            },
+            "name": f"{name}{suffix}",
+            "resourceversion": "v1",
+        },
+        "spec": {
+            "ports": [
+                {
+                    "name": "port",
+                    "port": port,
+                    "protocol": "TCP",
+                    "targetPort": port,
+                }
+            ],
+            "selector": {"name": name},
+        },
+    }
+
+
+def delete_tunneling_pod_and_svcs(v1, name, namespace):
+    v1.delete_namespaced_service(name=name, namespace=namespace)
+    v1.delete_namespaced_service(name=f"{name}-ssh", namespace=namespace)
+    v1.delete_namespaced_pod(name=name, namespace=namespace)
+
+
+def start_tunneling_pod_and_svcs(v1, name, namespace, image):
+    pod_manifest = {
+        "apiVersion": "v1",
+        "kind": "Pod",
+        "metadata": {
+            "labels": {
+                "name": name,
+            },
+            "name": name,
+            "resourceversion": "v1",
+        },
+        "spec": {
+            "containers": [
+                {
+                    "image": image,
+                    "imagePullPolicy": "Always",
+                    "name": name,
+                    "env": [
+                        {
+                            "name": "DEPLOYMENT_NAME",
+                            "value": name,
+                        },
+                        {
+                            "name": "DEPLOYMENT_NAMESPACE",
+                            "value": namespace,
+                        },
+                        {
+                            "name": "TUNNEL_SUPERUSER_PASS",
+                            "value": os.environ.get("TUNNEL_SUPERUSER_PASS"),
+                        },
+                        {
+                            "name": "SSHCONFIGFILE",
+                            "value": "/tmp/ssh_config",
+                        },
+                    ],
+                }
+            ],
+            "serviceAccount": "tunneling-devel-svc-acc",
+            "serviceAccountName": "tunneling-devel-svc-acc",
+        },
+    }
+    v1.create_namespaced_pod(body=pod_manifest, namespace=namespace)
+    while True:
+        resp = v1.read_namespaced_pod(name=name, namespace=namespace)
+        if resp.status.phase != "Pending":
+            break
+        time.sleep(1)
+    v1.create_namespaced_service(body=get_svc_manifest(name, 8080), namespace=namespace)
+    v1.create_namespaced_service(
+        body=get_svc_manifest(name, 2222, suffix="-ssh"), namespace=namespace
+    )
+
+
+def wait_for_tunneling_svc(url):
+    for _ in range(0, 40):
+        try:
+            r = requests.get(url=f"{url}/health/", timeout=2)
+            if r.status_code == 200:
+                break
+        except (
+            requests.exceptions.ConnectTimeout,
+            requests.exceptions.ConnectionError,
+            requests.exceptions.ReadTimeout,
+        ):
+            pass
+        time.sleep(1)
+
+
+def wait_for_tsi_svc(v1, name, namespace):
+    exec_command = ["/bin/sh"]
+    resp = stream(
+        v1.connect_get_namespaced_pod_exec,
+        name,
+        namespace,
+        command=exec_command,
+        stderr=True,
+        stdin=True,
+        stdout=True,
+        tty=False,
+        _preload_content=False,
+    )
+    command = "netstat -ltnp 2>/dev/null | tr -s ' ' | grep \":4433\" | wc -l"
+    while resp.is_open():
+        for _ in range(0, 150):
+            resp.write_stdin(command + "\n")
+            resp.update(timeout=1)
+            if resp.peek_stdout():
+                stdout = resp.read_stdout().strip()
+                if stdout == "1":
+                    resp.close()
+                    break
+            time.sleep(1)
+    if resp.is_open():
+        resp.close()
+
+
+def replace_ssh_host_in_tsi_manage_tunnel_script(
+    v1, name, namespace, tunnel_ssh_svc_name
+):
+    exec_command = [
+        "sed",
+        "-i",
+        "-e",
+        f"s/TUNNEL_SSH_HOST=localhost/TUNNEL_SSH_HOST={tunnel_ssh_svc_name}/g",
+        "/home/ljupyter/manage_tunnel.sh",
+    ]
+    resp = stream(
+        v1.connect_get_namespaced_pod_exec,
+        name,
+        namespace,
+        command=exec_command,
+        stderr=True,
+        stdin=False,
+        stdout=True,
+        tty=False,
+    )
+
+
+def add_test_files_to_tunneling(v1, name, namespace, data):
+    exec_command = ["/bin/sh"]
+    resp = stream(
+        v1.connect_get_namespaced_pod_exec,
+        name,
+        namespace,
+        command=exec_command,
+        stderr=True,
+        stdin=True,
+        stdout=True,
+        tty=False,
+        _preload_content=False,
+    )
+    commands = []
+    # Add authorized keys to allow unicore-test-tsi to connect to ljupyter
+    commands.append("mkdir -p /home/tunnel/.ssh\n")
+    commands.append("chown tunnel:users /home/tunnel/.ssh\n")
+    commands.append("chmod 700 /home/tunnel/.ssh\n")
+    for destination, inp, b64, chown, chmod in data:
+        if b64:
+            commands.append('echo -n "' + inp + '" | base64 -d >' + destination + "\n")
+        else:
+            commands.append('echo -n "' + inp + '\n" >' + destination + "\n")
+        if chown:
+            commands.append("chown " + chown + " " + destination + "\n")
+        if chmod:
+            commands.append("chmod " + chmod + " " + destination + "\n")
+    while resp.is_open():
+        resp.update(timeout=1)
+        if commands:
+            c = commands.pop(0)
+            resp.write_stdin(c)
+        else:
+            break
+    resp.close()
+
+
+def start_unicore_tsi_pod_and_svcs(v1, name, namespace, image, tunnel_ssh_svc):
+    pod_manifest = {
+        "apiVersion": "v1",
+        "kind": "Pod",
+        "metadata": {
+            "labels": {
+                "name": name,
+            },
+            "name": name,
+            "resourceversion": "v1",
+        },
+        "spec": {
+            "containers": [
+                {
+                    "image": image,
+                    "imagePullPolicy": "Always",
+                    "name": name,
+                    "env": [
+                        {
+                            "name": "TUNNEL_SSH_HOST",
+                            "value": tunnel_ssh_svc,
+                        },
+                    ],
+                }
+            ],
+            "imagePullSecrets": [{"name": "gitlab-registry"}],
+        },
+    }
+    v1.create_namespaced_pod(body=pod_manifest, namespace=namespace)
+    while True:
+        resp = v1.read_namespaced_pod(name=name, namespace=namespace)
+        if resp.status.phase != "Pending":
+            break
+        time.sleep(1)
+    v1.create_namespaced_service(
+        body=get_svc_manifest(name, 2223, suffix="-ssh"), namespace=namespace
+    )
+
+
+def delete_unicore_tsi_pod_and_svcs(v1, name, namespace):
+    v1.delete_namespaced_service(name=name, namespace=namespace)
+    v1.delete_namespaced_pod(name=f"{name}-ssh", namespace=namespace)
