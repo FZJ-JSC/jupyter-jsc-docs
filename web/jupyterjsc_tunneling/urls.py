3636811ae8391852277a02c6ea1579679a6b2f3e
kreuzer <t.kreuzer@fz-juelich.de>
2022-04-06 15:30:55 +0200

    add static files

--- web/jupyterjsc_tunneling/urls.py
+++ web/jupyterjsc_tunneling/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.http import HttpResponse
 from django.urls import include
 from django.urls import path
@@ -31,3 +32,5 @@ urlpatterns = [
     path("api/logs/", include("logs.urls")),
     path("api-auth/", include("rest_framework.urls")),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
