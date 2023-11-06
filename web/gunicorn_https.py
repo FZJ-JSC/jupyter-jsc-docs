524593d5258d4753c0abe972a2d14630c079701f
Tim Kreuzer <t.kreuzer@fz-juelich.de>
2023-11-06 15:44:25 +0100

    suppress health logs

--- web/gunicorn_https.py
+++ web/gunicorn_https.py
@@ -160,6 +160,27 @@ loglevel = "info"
 accesslog = "-"
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
+import logging
+import re
+
+
+class RequestPathFilter(logging.Filter):
+    def __init__(self, *args, path_re, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.path_filter = re.compile(path_re)
+
+    def filter(self, record):
+        req_path = record.args["U"]
+        if not self.path_filter.match(req_path):
+            return True  # log this entry
+        # ... additional conditions can be added here ...
+        return False  # do not log this entry
+
+
+def on_starting(server):
+    server.log.access_log.addFilter(RequestPathFilter(path_re=r"^/api/health/$"))
+
+
 #
 # Process naming
 #
